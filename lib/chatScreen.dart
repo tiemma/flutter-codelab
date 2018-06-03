63096e50c639704c09cce23657e4a2e2f726a030
blank <bakareemmanuel98@outlook.com>
2018-06-03 01:12:38 +0100

    Removed libraries from master

--- lib/chatScreen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'chatScreenState.dart';
-
-
-class ChatScreen extends StatefulWidget {
-
-  @override
-  State createState() => new ChatScreenState() ;
-}
